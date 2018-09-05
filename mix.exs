b7b52cb98f6e3c5e10bf12e81154d8a36c59d9a0
Robert Juncosa <bobbyjuncosa@gmail.com>
2018-09-05 14:31:24 -0400

    [release] v0.3.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule AccessDecisionManager.MixProject do
   use Mix.Project
 
-  @version "0.2.1"
+  @version "0.3.0"
   @github_url "https://github.com/bjunc/access-decision-manager"
 
   def project do
